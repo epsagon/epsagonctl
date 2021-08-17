fe132b970534471f693779b4433979e965ce5e86
ronnathaniel <ronnathaniel>
2021-08-17 10:01:48 -0400

    fix(__main__): splut up opts

--- epsagonctl/__main__.py
+++ epsagonctl/__main__.py
@@ -1,24 +1,33 @@
 #! /usr/bin/python3
 
-from epsagoncli.consts import (
-    VARS_COMMAND, VARS_SUBCOMMAND
+from epsagonctl.consts import (
+    VARS_COMMAND, VARS_SUBCOMMAND, VARS_DEFAULT,
 )
-from epsagoncli.utils import (
+from epsagonctl.utils import (
     _parse_args, _list_users, _list_groups, _invite_user
 )
 
+options = {
+    'iam': {
+        'invite-user': _invite_user,
+        'list-users': _list_users,
+        'list-groups': _list_groups,
+    },
+    VARS_DEFAULT: {
+        VARS_DEFAULT: lambda: None
+    },
+}
+
 
 def run():
-    cmd = _parse_args()
-    res = {
-        'iam': {
-            'invite-user': _invite_user,
-            'list-users': _list_users,
-            'list-groups': _list_groups,
-        },
-    }[cmd[VARS_COMMAND]][cmd[VARS_SUBCOMMAND]]()
+    args = _parse_args()
+    res = options.get(
+        args.get(VARS_COMMAND, options[VARS_DEFAULT])
+    ).get(
+        args.get(VARS_SUBCOMMAND, options[VARS_DEFAULT][VARS_DEFAULT])
+    )()
 
-    print(res.status_code, res.text)
+    print(res.json())
 
 
 if __name__ == '__main__':
